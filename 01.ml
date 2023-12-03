3a10aa2ee1b26f9d297f67ca4e086c89663af419
Anton Lavrov <anton.lavrov@oracle.com>
2023-12-03 07:47:06 -0800

    Day 1 (in OCaml)

---
+++ 01.ml
@@ -0,0 +1,27 @@
+open Common
+open Printf
+
+let rec find_digit digits_map line : int =
+    let c = String.get line 0 in
+    if c >= '0' && c <= '9'
+        then Char.code c - Char.code '0'
+        else
+            let m = List.find_opt (fun (name, _) -> String.starts_with ~prefix:name line) digits_map in
+            if Option.is_some m
+                then snd (Option.get m)
+                else find_digit digits_map (Str.string_after line 1)
+
+let find_digits digits_map line : int =
+    let reverse_digits_map = List.map (on_fst reverse_string) digits_map in
+    let d1 = find_digit digits_map line in
+    let d2 = find_digit reverse_digits_map (reverse_string line) in
+    d1 * 10 + d2
+
+let lines = read_lines (input_file "01.input")
+
+let ds = sum (List.map (find_digits []) lines)
+let () = printf "%d\n" ds
+
+let digits = [("one", 1); ("two", 2); ("three", 3); ("four", 4); ("five", 5); ("six", 6); ("seven", 7); ("eight", 8); ("nine", 9)]
+let ds = sum (List.map (find_digits digits) lines)
+let () = printf "%d\n" ds
